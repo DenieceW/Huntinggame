e17f009932c80b3dee64864a2c149f3386a9d4ad
Deniece <52138289+DenieceW@users.noreply.github.com>
2020-06-24 15:35:52 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,29 +6,5 @@ public class Main {
         HuntingGame huntingGame = new HuntingGame();
         huntingGame.startHunting();
 
-
-
-
-
-
-
-//        Player player = new Player();
-//        System.out.println("Player++++++++++++++++++++");
-//        player.damageForce();
-//        System.out.print("Attack damage: ");
-//        player.getDamage();
-//        System.out.println("HP: " + player.healthStatus());
-//        System.out.println("PotionHP: " + player.potionHealth());
-//        System.out.println("Take Potion: " + player.getPotion());
-//
-//        System.out.println("Enemy++++++++++++++++++++");
-//
-//        Enemy enemy = new Enemy();
-//        System.out.println("Attack damage: " + enemy.setAttackDamage());
-//        System.out.println("HP: "+ enemy.setHP());
-
-
-
-
     }
 }
